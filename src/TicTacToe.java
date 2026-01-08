4bbff8f6261200881fb7d8058f70c91d1418ea7e
LeeringFish <theleeringfish@proton.me>
2026-01-07 20:00:55 -0500

    add label

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,9 +7,26 @@ public class TicTacToe {
     int boardHeight = 650;
 
     JFrame frame = new JFrame();
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
 
     TicTacToe() {
-        frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.darkGray);
+        textLabel.setForeground(Color.white);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+        frame.setVisible(true);
     }
 }
